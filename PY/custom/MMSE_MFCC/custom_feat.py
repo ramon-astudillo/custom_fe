c0f195efd53f78982808fb854001fda7bf0b2e50
Ramón Astudillo <ramon@astudillo.com>
2015-02-09 23:43:51 +0000

    First functional version of the Python features

--- PY/custom/MMSE_MFCC/custom_feat.py
+++ PY/custom/MMSE_MFCC/custom_feat.py
@@ -4,21 +4,19 @@
 Template for custom feature extraction 
 '''
 
-#import ipdb
 import numpy as np
 #import matplotlib.pyplot as plt
 
 import re
 
 # DEBUG
-#from ipdb import set_trace()
+#from ipdb import set_trace
 
 # Add the needed tools
 import interfaces.audio as ia        # Read raw 
 import interfaces.htk as htk         # Read raw 
-import interfaces.audimus as aud     
 import processing.signal as sip      # STFT
-import processing.imcra as ns        # IMCRA
+import processing.imcra as se        # IMCRA
 import processing.features as fe     # MFCCs
 import os
 # Template for feature extraction
@@ -26,29 +24,33 @@ import interfaces.HCo as HCopy
 
 # SUPPORTED HTK FIELDS AND DEFAULT VALUES IF ADMITTED
 HTK_SUPP = {
-    'windowsize' : None, 
-    'deltawindow': None,
-    'accwindow'  : None,  
-    'targetkind' : 'USER', 
-    'preemcoef'  : 0.97, 
-    'usepower'   : False, 
-    'numchans'   : 23,
-    'numceps'    : 12, 
-    'ceplifter'  : 22,
+    'windowsize'   : None, 
+    'deltawindow'  : None,
+    'accwindow'    : None,  
+    'targetkind'   : 'USER', 
+    'targetformat' : 'HTK',
+    'preemcoef'    : 0.97, 
+    'usepower'     : False, 
+    'numchans'     : 23,
+    'numceps'      : 12, 
+    'ceplifter'    : 22,
            }
 
 # ADDITIONALLY NON-HTK SUPPORTED FIELDS AND DEFAULT VALUES IF ADMITTED
 EXTRA_SUPP = {
-    'fp'            : None,
-    'tc'            : None,
-    'in_fs'         : None,
-    'work_fs'       : None,
-    'shift'         : None,
-    'nfft'          : None,
-    'byteorder_raw' : 'littleendian',
-    'do_resume'     : 0,
-    'do_debug'      : 0,
-    'do_up'         : 0,
+    'fp'              : None,
+    'tc'              : None,
+    'in_fs'           : None,
+    'work_fs'         : None,
+    'shift'           : None,
+    'nfft'            : None,
+    'separate_events' : 0,     
+    'byteorder_raw'   : 'littleendian',
+    'mmse_method'     : 'LSA',
+    'init_time'       : 0.1,
+    'do_resume'       : 0,
+    'do_debug'        : 0,
+    'do_up'           : 0
              }
 
 def validate_config(config):
@@ -77,6 +79,7 @@ def validate_config(config):
     return config
 
 class FE():
+
     def __init__(self, config):
     
         # Set in_fs to be work_fs if not specified
@@ -94,7 +97,7 @@ class FE():
             raise NotImplementedError, "Not implemented yet"
     
         # IMCRA
-        self.se   = ns.imcra_se(config['nfft'])
+        self.se   = se.imcra_se(config['nfft'])
 
         # Initialize MFCC coefficients
         self.mfcc =  fe.mfcc(config['work_fs'], config['nfft'], 
@@ -105,80 +108,74 @@ class FE():
         '''
         Feature extraction
         '''
-        # Get indices for the position of speech and background based on 
-        # external info. If MLF or STM provided for VAD use them.
-        if 'stm_vad' in self.config:
+    
+        # Read this audio file
+        y_t = ia.read(src_file, in_fs=self.config['in_fs'], 
+                      out_fs=self.config['work_fs'])[0]
 
-            # VAD SPECIFIED BY A STM
-            if src_file not in self.config['stm_trans']:
-                raise EnvironmentError, ("stm file %s has not transcription "
-                                         "for %s" % (self.config['stm_vad'], 
-                                         src_file))  
-            # Collect speech events and preceeding backgrounds
-            events = []
-            backgs = []
-            t_bg   = 0 
-            for tr in self.config['stm_trans'][src_file]:
-                # Preceeding background
-                if not tr[2]:
-                    backgs.append(None) 
-                else:
-                    backgs.append((0, tr[2]*self.config['in_fs'])) 
-                # Speech event
-                events.append((src_file, tr[2]*self.config['in_fs'], 
-                               tr[3]*self.config['in_fs'])) 
-            
- 
-        else:
+        #
+        # BEAMFORMING
+        # 
 
-            # ONE SINGLE EVENT IN PRESENT MICROPHONE
-            if 'T_init' in config:
-                events = [(src_file, 0, -1)]  
-                backgs = None 
-            else:
-                events = [(src_file, config['T_init'], -1)]  
-                backgs = [(0, config['T_init'])] 
+        # Beamformer poiting at the fromtal direction by default
+        if y_t.shape[1] > 1:
+            y_t = y_t.sum(1)
 
-        # Loop over events in the scene
-        for backg, event in zip(backgs, events):
-     
-            # Read this audio file
-            y_t = HCopy.read(event[0], self.config['in_fs'], 
-                             self.config['work_fs'])
-
-            # Select segment of background preceeding speech 
-            if backg:
-                d_t = y_t[backg[0], backg[1]]
-            else:
-                d_t = None
-
-            # Select segment of speech 
-            y_t = y_t[event[1]:event[2]] 
+        #
+        # SPEECH ENHANCEMENT
+        #            
         
-            # Pre-emphasis, STFT
-            y_t = sip.preemphasis(y_t, coef=self.config['preemcoef'])
-            Y   = sip.stft(y_t, self.config['windowsize'], self.config['shift'], 
-                           self.config['nfft'])
+        # Pre-emphasis, STFT
+        y_t = sip.preemphasis(y_t, coef=self.config['preemcoef'])
+        Y   = sip.stft(y_t, self.config['windowsize'], self.config['shift'], 
+                       self.config['nfft'])
+        # Compute IMCRA  
+        hat_X_LSA = self.se.update(Y)
 
 
-            #
-            # SPEECH ENHANCEMENT
-            #            
+        if (self.config['mmse_method'] == 'MFCC' or 
+            self.config['mmse_method'] == 'Wiener'):
 
+            # Get a priori SNR and noise variance 
+            xi, Lambda_D = self.se.get_param(['xi', 'Lambda_D'])
+            # Get Wiener estimate and residual MSE
+            G     = xi/(1+xi)
+            # Use the posterior associated to the Wiener filter
+            hat_X = G*Y
+            if self.config['mmse_method'] == 'MFCC':
+                MSE   = G*Lambda_D 
+            else:
+                MSE   = np.zeros(G.shape) 
 
-            # MFCC
-            x   = self.mfcc.extract(Y)  
-            # CMS
-            x   = self.mfcc.cms(x)
-            # Deltas, Accelerations
-            d   = fe.deltas(x)   
-            a   = fe.deltas(d)   
-            x   = np.concatenate((x, d, a))
+        elif self.config['mmse_method'] == 'LSA':
+            
+            # Use LSA 
+            hat_X = hat_X_LSA
+            MSE   = np.zeros(hat_X.shape)
+
+        #
+        # FEATURE EXTRACTION / UNCERTAINTY PROPAGATION
+        #
+
+        # MFCC
+        mu_x, Sigma_x = self.mfcc.extract_up(hat_X, MSE)  
+        # CMS
+        mu_x, Sigma_x   = self.mfcc.cms_up(mu_x, Sigma_x)
+        # Deltas, Accelerations
+        mu_d, Sigma_d = fe.deltas_up(mu_x, Sigma_x)   
+        mu_a, Sigma_a = fe.deltas_up(mu_d, Sigma_d)   
+        mu_x          = np.concatenate((mu_x, mu_d, mu_a))
+        Sigma_x       = np.concatenate((Sigma_x, Sigma_d, Sigma_a))
+
+        # Provide uncertainty
+        if self.config['do_up']:
+            x = np.concatenate((mu_x, Sigma_x))
+        else:
+            x = mu_x
        
-            # Plot features, for debug 
-            HCopy.plot(x)
-     
-            # Write features
-            y_t = HCopy.write(tgt_file, x, self.config['fp'], self.config['tc'])        
-
-            # TODO: Write vad file
+        # Plot features, for debug 
+        if self.config['targetformat'] == 'HTK':
+            htk.writehtkfeats(tgt_file, x, self.config['fp'], self.config['tc']) 
+        else:
+            raise ValueError, ("TARGETFORMAT = %s Not supported" 
+                               % self.config['targetformat'])
